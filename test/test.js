9b6cc5b15f8cb64e6a75878e28875c5d608c62a0
LabMemNo003 <LabMemNo003@gmail.com>
2020-06-09 14:02:52 +0800

    Make test compatible to new api

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const http = require('http');
 const request = require('supertest');
 const expect = require('chai').expect;
 const { Pipeline, Branch } = require('nahan-onion');
-const Path = require('..');
+const PathFull = require('..').PathFull;
 
 describe('nahan-path', () => {
 
@@ -15,32 +15,32 @@ describe('nahan-path', () => {
                 ctx.res.end();
             },
             Branch(
-                Path('/'),
-                async ctx => ctx.res.write('Path (string): /')
+                PathFull('/'),
+                async ctx => ctx.res.write('PathFull (string): /')
             ),
             Branch(
-                Path('/test'),
-                async ctx => ctx.res.write('Path (string): /test')
+                PathFull('/test'),
+                async ctx => ctx.res.write('PathFull (string): /test')
             ),
             Branch(
-                Path(new String('/test/String')),
-                async ctx => ctx.res.write('Path (string): /test/String')
+                PathFull(new String('/test/String')),
+                async ctx => ctx.res.write('PathFull (string): /test/String')
             ),
             Branch(
-                Path(/\/test\/(\d+)/),
-                async (ctx, _, matches) => ctx.res.write('Path (RegExp): /test/' + matches[1])
+                PathFull(/\/test\/(\d+)/),
+                async (ctx, _, matches) => ctx.res.write('PathFull (RegExp): /test/' + matches[1])
             ),
             Branch(
-                Path(/\/test\/(\d+)\/(\d+)/),
-                async (ctx, _, matches) => ctx.res.write('Path (RegExp): /test/' + matches[1] + '/' + matches[2])
+                PathFull(/\/test\/(\d+)\/(\d+)/),
+                async (ctx, _, matches) => ctx.res.write('PathFull (RegExp): /test/' + matches[1] + '/' + matches[2])
             ),
             Branch(
-                Path(/\/test\/(?<s1>[a-zA-Z]+)/),
-                async (ctx, _, groups) => ctx.res.write('Path (RegExp): /test/' + groups.s1)
+                PathFull(/\/test\/(?<s1>[a-zA-Z]+)/),
+                async (ctx, _, groups) => ctx.res.write('PathFull (RegExp): /test/' + groups.s1)
             ),
             Branch(
-                Path(/\/test\/(?<s1>[a-zA-Z]+)\/(?<s2>[a-zA-Z]+)/),
-                async (ctx, _, groups) => ctx.res.write('Path (RegExp): /test/' + groups.s1 + '/' + groups.s2)
+                PathFull(/\/test\/(?<s1>[a-zA-Z]+)\/(?<s2>[a-zA-Z]+)/),
+                async (ctx, _, groups) => ctx.res.write('PathFull (RegExp): /test/' + groups.s1 + '/' + groups.s2)
             ),
             async ctx => ctx.res.write('Other path: ' + ctx.path)
         );
@@ -49,42 +49,42 @@ describe('nahan-path', () => {
 
     const agent = request.agent(server);
 
-    describe('Path (string): /', () => {
-        it('GET /', done => { agent.get('/').expect('Path (string): /', done) });
-        it('GET //', done => { agent.get('//').expect('Path (string): /', done) });
-        it('GET ///', done => { agent.get('///').expect('Path (string): /', done) });
+    describe('PathFull (string): /', () => {
+        it('GET /', done => { agent.get('/').expect('PathFull (string): /', done) });
+        it('GET //', done => { agent.get('//').expect('PathFull (string): /', done) });
+        it('GET ///', done => { agent.get('///').expect('PathFull (string): /', done) });
     });
 
-    describe('Path (string): /test', () => {
-        it('GET /test', done => { agent.get('/test').expect('Path (string): /test', done) });
-        it('GET /test/', done => { agent.get('/test/').expect('Path (string): /test', done) });
-        it('GET //test//', done => { agent.get('//test//').expect('Path (string): /test', done) });
+    describe('PathFull (string): /test', () => {
+        it('GET /test', done => { agent.get('/test').expect('PathFull (string): /test', done) });
+        it('GET /test/', done => { agent.get('/test/').expect('PathFull (string): /test', done) });
+        it('GET //test//', done => { agent.get('//test//').expect('PathFull (string): /test', done) });
     });
 
-    describe('Path (String): /test/String', () => {
-        it('GET /test/String', done => { agent.get('/test/String').expect('Path (string): /test/String', done) });
-        it('GET /test/String/', done => { agent.get('/test/String/').expect('Path (string): /test/String', done) });
-        it('GET //test//String//', done => { agent.get('//test//String//').expect('Path (string): /test/String', done) });
+    describe('PathFull (String): /test/String', () => {
+        it('GET /test/String', done => { agent.get('/test/String').expect('PathFull (string): /test/String', done) });
+        it('GET /test/String/', done => { agent.get('/test/String/').expect('PathFull (string): /test/String', done) });
+        it('GET //test//String//', done => { agent.get('//test//String//').expect('PathFull (string): /test/String', done) });
     });
 
-    describe('Path (RegExp): ' + /\/test\/(\d+)/.source, () => {
-        it('GET /test/1', done => { agent.get('/test/1').expect('Path (RegExp): /test/1', done) });
-        it('GET //test//123//', done => { agent.get('//test//123//').expect('Path (RegExp): /test/123', done) });
+    describe('PathFull (RegExp): ' + /\/test\/(\d+)/.source, () => {
+        it('GET /test/1', done => { agent.get('/test/1').expect('PathFull (RegExp): /test/1', done) });
+        it('GET //test//123//', done => { agent.get('//test//123//').expect('PathFull (RegExp): /test/123', done) });
     });
 
-    describe('Path (RegExp): ' + /\/test\/(\d+)\/(\d+)/.source, () => {
-        it('GET /test/1/9', done => { agent.get('/test/1/9').expect('Path (RegExp): /test/1/9', done) });
-        it('GET //test//123//789//', done => { agent.get('//test//123//789//').expect('Path (RegExp): /test/123/789', done) });
+    describe('PathFull (RegExp): ' + /\/test\/(\d+)\/(\d+)/.source, () => {
+        it('GET /test/1/9', done => { agent.get('/test/1/9').expect('PathFull (RegExp): /test/1/9', done) });
+        it('GET //test//123//789//', done => { agent.get('//test//123//789//').expect('PathFull (RegExp): /test/123/789', done) });
     });
 
-    describe('Path (RegExp): ' + /\/test\/(?<s1>[a-zA-Z]+)/.source, () => {
-        it('GET /test/a', done => { agent.get('/test/a').expect('Path (RegExp): /test/a', done); });
-        it('GET //test//abc//', done => { agent.get('//test//abc//').expect('Path (RegExp): /test/abc', done) });
+    describe('PathFull (RegExp): ' + /\/test\/(?<s1>[a-zA-Z]+)/.source, () => {
+        it('GET /test/a', done => { agent.get('/test/a').expect('PathFull (RegExp): /test/a', done); });
+        it('GET //test//abc//', done => { agent.get('//test//abc//').expect('PathFull (RegExp): /test/abc', done) });
     });
 
-    describe('Path (RegExp): ' + /\/test\/(?<s1>[a-zA-Z]+)\/(?<s2>[a-zA-Z]+)/.source, () => {
-        it('GET /test/a/z', done => { agent.get('/test/a/z').expect('Path (RegExp): /test/a/z', done); });
-        it('GET //test//abc//xyz//', done => { agent.get('//test//abc//xyz//').expect('Path (RegExp): /test/abc/xyz', done) });
+    describe('PathFull (RegExp): ' + /\/test\/(?<s1>[a-zA-Z]+)\/(?<s2>[a-zA-Z]+)/.source, () => {
+        it('GET /test/a/z', done => { agent.get('/test/a/z').expect('PathFull (RegExp): /test/a/z', done); });
+        it('GET //test//abc//xyz//', done => { agent.get('//test//abc//xyz//').expect('PathFull (RegExp): /test/abc/xyz', done) });
     });
 
     describe('Other path', () => {
@@ -95,9 +95,9 @@ describe('nahan-path', () => {
 
     describe('Error handler', () => {
         it('Error type of "pattern"', () => {
-            const Path_123 = Path.bind(null, 123);
-            expect(Path_123).to.throw(TypeError);
-            expect(Path_123).to.throw('Type of "pattern" should be "string", "String" or "RegExp"');
+            const PathFull_123 = PathFull.bind(null, 123);
+            expect(PathFull_123).to.throw(TypeError);
+            expect(PathFull_123).to.throw('Type of "pattern" should be "string", "String" or "RegExp"');
         });
     });
 });
